6d43ff7f1bad2153c2c33b3ca5c6a5c823f6d932
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-06-02 22:46:54 +0300

    fix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -27,14 +27,23 @@ const ContactForm = () => {
   const handleSubmit = evt => {
     evt.preventDefault();
 
-    
-    if (!contacts.find(el => el.name === name)) {
-      dispatch(addContact({ name: name, phone: number}));
+    const existingContact = contacts.find(el => el.name.toLowerCase() === name.toLowerCase());
+    if (!existingContact) {
+      dispatch(addContact({ name: name, phone: number }));
       setName('');
       setNumber('');
     } else {
       alert(`${name} is already in contacts`);
     }
+
+    
+    // if (!contacts.find(el => el.name === name)) {
+    //   dispatch(addContact({ name: name, phone: number}));
+    //   setName('');
+    //   setNumber('');
+    // } else {
+    //   alert(`${name} is already in contacts`);
+    // }
   };
 
   return (
